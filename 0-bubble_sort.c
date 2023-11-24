92074b14d0033fa0ce42dbd6b478a5f782ed228d
rodynaamrfathy <rodynaamr@icloud.com>
2023-11-24 09:01:44 -0800

    ‘hi’

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorting an array using bubble method
+ * @array: pointer to an array of integers
+ * @size: int size of array
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	long unsigned int i = 0, j = 0;
+	int temp;
+
+	for (i = 0; i < (size-1); i++)
+	{
+		for (j = 0; j < (size-1); j++)
+		{
+			/*swap*/
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+			print_array(array, size);
+		}
+	}
+}
